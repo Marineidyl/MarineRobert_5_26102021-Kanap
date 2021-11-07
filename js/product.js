66849b12872ffb3ff0a5dc01de900967e3727176
Marineidyl <marine.rbt51100@gmail.com>
2021-11-07 15:10:07 +0100

    test api products -> promise and adding script in the page index.html

--- js/product.js
+++ js/product.js
@@ -2,8 +2,8 @@
 let productsData = [];
 
 const fetchProducts = async () => {
-    await fetch("http://localhost:3000/api/products").then((res) => console.log(res.json()),
-    );
+    await fetch("http://localhost:3000/api/products").then((res) =>  res.json())
+    .then((promise) => console.log(promise))
 };
 
 fetchProducts();
